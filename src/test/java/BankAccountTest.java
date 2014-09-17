2059628dcc2d2f8de06043eec656ca7bd66a8762
dianagamez <diana.gamez.fon@gmail.com>
2014-09-17 09:55:50 -0500

    good version

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -26,12 +26,5 @@ public class BankAccountTest {
 		assertEquals(acc.getBalance(), -15);
 	}
 	
-	@Test
-	public void testWithdrawWithNoPenalty() {
-		BankAccount acc = new BankAccount(10);
-		acc.withdraw(20);
-		assertEquals(acc.getBalance(), -10);
-	}
-	
 	
 }
