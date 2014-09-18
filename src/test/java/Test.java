df0c7ac8f1d90965f33c7fb4653be021587b90e4
dianagamez <diana.gamez.fon@gmail.com>
2014-09-18 11:56:26 -0500

    test updated

---
+++ src/test/java/Test.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+
+public class Test {
+
+	@org.junit.Test
+	public void testDeposit() {
+		BankAccount acc = new BankAccount();
+		acc.deposit(50);
+		assertEquals(acc.getBalance(), 50);
+	}
+	
+	@org.junit.Test
+	public void testWithdraw() {
+		BankAccount acc = new BankAccount(75);
+		acc.withdraw(50);
+		assertEquals(acc.getBalance(), 25);
+	}
+	
+	@org.junit.Test
+	public void testWithdrawWithPenalty() {
+		BankAccount acc = new BankAccount(10);
+		acc.withdraw(20);
+		assertEquals(acc.getBalance(), -15);
+	}
+}
